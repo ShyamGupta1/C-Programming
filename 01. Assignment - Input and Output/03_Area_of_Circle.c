4533c87e8d587e9238b4383597e269fec47beaed
ShyamGupta1 <shyamgupta4004@gmail.com>
2024-09-06 19:36:00 +0530

    Problem No. 3

---
+++ 01. Assignment - Input and Output/03_Area_of_Circle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int radius;
+    printf("Enter radius: ");
+    scanf("%d", &radius);
+    printf("Area of circle is %.2f having radius %d", 22.0 / 7 * radius * radius, radius);
+    printf("\n");
+    return 0;
+}
