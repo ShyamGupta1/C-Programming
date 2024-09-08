e3d6f6a75527da4d52825bad9d3c577cef7d7f68
ShyamGupta1 <shyamgupta4004@gmail.com>
2024-09-08 17:37:15 +0530

    Problem No. 4

---
+++ 02. Assignment - Operators/04_Swap_Without_Third_Variable.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d%d", &a, &b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("After Swapping:\na - %d\nb - %d", a, b);
+    printf("\n");
+    return 0;
+}
+
