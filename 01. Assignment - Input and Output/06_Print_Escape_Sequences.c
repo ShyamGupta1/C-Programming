506466dd72a4bb4cf1cf44bf708724f972e53264
ShyamGupta1 <shyamgupta4004@gmail.com>
2024-09-08 17:05:15 +0530

    Problem No. 6

---
+++ 01. Assignment - Input and Output/06_Print_Escape_Sequences.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%%d\n");
+    printf("\\n\n");
+    printf("\\t\n");
+    printf("\\b\n");
+    printf("\\r\n");
+    printf("\\f\n");
+    printf("\\\n");
+    printf("\\'\n");
+    printf("\\\"\n");
+
+    return 0;
+}
