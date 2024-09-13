982c199f572c33f8371cacffa1c32199a398002a
Karl_X_ <shyamgupta4004@gmail.com>
2024-09-13 22:59:55 +0530

    Problem No. 11

---
+++ 04. Assignment - Iterative Control Statements/11_Print_Hello_World_N_Times.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int N, i = 0;
+    printf("Enter N: ");
+    scanf("%d", &N);
+    while (i < N)
+    {
+        printf("Hello World!\n");
+        i++;
+    }
+
+    return 0;
+}
