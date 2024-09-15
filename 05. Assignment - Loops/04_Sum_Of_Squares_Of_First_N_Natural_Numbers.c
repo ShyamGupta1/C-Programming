21deca0482a80d782fed93176072479b4b5f9dea
Karl_X_ <shyamgupta4004@gmail.com>
2024-09-15 19:58:12 +0530

    Problem No. 4

---
+++ 05. Assignment - Loops/04_Sum_Of_Squares_Of_First_N_Natural_Numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+        sum = sum + i * i;
+
+    printf("Sum of squares of first %d natural numbers is: %d", n, sum);
+
+    return 0;
+}
