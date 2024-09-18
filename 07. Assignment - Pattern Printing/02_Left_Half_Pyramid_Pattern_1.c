18f6b5ef2ef98aee0cdc406575a9fb365c47199b
Karl_X_ <shyamgupta4004@gmail.com>
2024-09-18 22:39:09 +0530

    Problem No. 2

---
+++ 07. Assignment - Pattern Printing/02_Left_Half_Pyramid_Pattern_1.c
@@ -0,0 +1,30 @@
+/*
+
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a Number: ");
+    scanf("%d", &n);
+    for (int row = 1; row <= n; row++)
+    {
+        // Spaces
+        for (int col = 1; col <= n - row; col++)
+            printf("  ");
+        // Stars
+        for (int col = 1; col <= row; col++)
+            printf("* ");
+        printf("\n");
+    }
+
+    return 0;
+}
