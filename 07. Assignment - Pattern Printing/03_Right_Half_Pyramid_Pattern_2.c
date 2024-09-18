81f851bfe20db965d5cf1bc6ac1c08a2c4f5b01a
Karl_X_ <shyamgupta4004@gmail.com>
2024-09-18 22:40:35 +0530

    Problem No. 3

---
+++ 07. Assignment - Pattern Printing/03_Right_Half_Pyramid_Pattern_2.c
@@ -0,0 +1,24 @@
+/*
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a Number: ");
+    scanf("%d", &n);
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = 1; col <= row; col++)
+            printf("%d ", col);
+        printf("\n");
+    }
+
+    return 0;
+}
