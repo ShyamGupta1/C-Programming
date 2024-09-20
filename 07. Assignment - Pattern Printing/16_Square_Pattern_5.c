c63bf438ac67dcca022e47320ae65e21c3161fe3
ShyamGupta1 <shyamgupta4004@gmail.com>
2024-09-20 23:43:03 +0530

    Problem No .16

---
+++ 07. Assignment - Pattern Printing/16_Square_Pattern_5.c
@@ -0,0 +1,25 @@
+/*
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+5 4 3 2 1
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a Number: ");
+    scanf("%d", &n);
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = n; col >= 1; col--)
+            printf("%d ", col);
+
+        printf("\n");
+    }
+
+    return 0;
+}
