8340c411e910481e7c4bec501dc10b5ba030aaf0
ShyamGupta1 <shyamgupta4004@gmail.com>
2024-09-22 00:29:55 +0530

    Problem No. 22

---
+++ 07. Assignment - Pattern Printing/22_Inverted_Full_Pyramid_Pattern.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a Number: ");
+    scanf("%d", &n);
+    for (int row = 1; row <= n; row++)
+    {
+        // Spaces
+        for (int col = 1; col <= row - 1; col++)
+            printf("  ");
+        // Stars
+        for (int col = 2 * n - (2 * row - 1); col >= 1; col--)
+            printf("* ");
+        printf("\n");
+    }
+
+    return 0;
+}
