a2f37a0f81e8c6397c999991893c416330da367e
ShyamGupta1 <shyamgupta4004@gmail.com>
2024-09-23 01:26:41 +0530

    Problem No. 25

---
+++ 07. Assignment - Pattern Printing/25_Inverted_Right_Half_Pyramid_Pattern_1.c
@@ -0,0 +1,25 @@
+/*
+ * * * * *
+ * * * *
+ * * *
+ * *
+ *
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a Number: ");
+    scanf("%d", &n);
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = 1; col <= n - (row - 1); col++)
+
+            printf("* ");
+        printf("\n");
+    }
+
+    return 0;
+}
