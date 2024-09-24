74d0d00e0657d08947b264a66d4bdad083d3d7ba
ShyamGupta1 <shyamgupta4004@gmail.com>
2024-09-24 21:24:00 +0530

    Problem No. 30

---
+++ 07. Assignment - Pattern Printing/30_Hollow_Right_Triangle.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a Number: ");
+    scanf("%d", &n);
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            if (col == 1 || col == row || row == n)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
