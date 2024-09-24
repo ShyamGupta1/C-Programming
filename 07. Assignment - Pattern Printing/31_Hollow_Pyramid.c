38611d1c62862221ce12ebe48f83d4e1336d80b0
ShyamGupta1 <shyamgupta4004@gmail.com>
2024-09-24 21:25:28 +0530

    Problem No. 31

---
+++ 07. Assignment - Pattern Printing/31_Hollow_Pyramid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a Number: ");
+    scanf("%d", &n);
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = 1; col <= n - row; col++)
+            printf("  ");
+        for (int col = 1; col <= 2 * row - 1; col++)
+        {
+            if (col == 1 || col == 2 * row - 1 || row == n)
+                printf("* ");
+            else
+                printf("  ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
