12c52919ba4f71c77c7b9b237a33baa6e7a2da15
ShyamGupta1 <shyamgupta4004@gmail.com>
2024-09-30 22:00:29 +0530

    Problem No. 4

---
+++ 15. Assignment - Multi-Dimensional Array/04_Sum_Of_Right_Diagonal.c
@@ -0,0 +1,33 @@
+// Write a program in C to find the sum of right diagonals of a matrix.
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j, sum = 0, arr[3][3];
+    printf("Enter elements of matrix:\n");
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+        {
+            printf("Enter element a%d%d: ", i + 1, j + 1);
+            scanf("%d", &arr[i][j]);
+        }
+    // Print Matrix
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+        {
+            printf("%5d", arr[i][j]);
+            if (j == 2)
+                printf("\n\n");
+        }
+    // Sum of Diagonal Elements
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+        {
+            if (i == j)
+                sum = sum + arr[i][j];
+        }
+    printf("Sum is: %d", sum);
+
+    return 0;
+}
