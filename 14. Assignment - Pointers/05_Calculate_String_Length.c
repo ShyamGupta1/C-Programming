34458dca5dfaf55d5e59b0755d36d7183e15d5c4
Shyam Gupta <shyamgupta4004@gmail.com>
2024-10-16 07:04:52 +0530

    Problem No. 5

--- 14. Assignment - Pointers/05_Calculate_String_Length.c
+++ 14. Assignment - Pointers/05_Calculate_String_Length.c
@@ -1,18 +1,18 @@
 // Write a program to calculate the length of the string using a pointer
 
 #include <stdio.h>
-int str_Length(char *, int size);
+int str_Length(char *);
 int main()
 {
     char str[20];
     printf("Enter a String: ");
     fgets(str, 20, stdin);
-    str[str_Length(str, 20) - 1] = '\0';
-    printf("String Length: %d", str_Length(str, 20));
+    str[str_Length(str) - 1] = '\0';
+    printf("String Length: %d", str_Length(str));
 
     return 0;
 }
-int str_Length(char *a, int size)
+int str_Length(char *a)
 {
     int i;
     for (i = 0; *(a + i); i++)
