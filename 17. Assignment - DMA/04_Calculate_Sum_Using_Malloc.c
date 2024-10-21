e0b6b343e59de922abdb03236004ad27a5624bc1
Shyam Gupta <shyamgupta4004@gmail.com>
2024-10-21 06:14:25 +0530

    Problem No. 4

---
+++ 17. Assignment - DMA/04_Calculate_Sum_Using_Malloc.c
@@ -0,0 +1,25 @@
+// Write a program to calculate the sum of n numbers entered by the user using malloc and free.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int Sum(int *arr, int size)
+{
+    int i, sum = 0;
+    for (i = 0; i < size; i++)
+        sum = sum + *(arr + i);
+    return sum;
+}
+
+int main()
+{
+    int i, size, *ptr;
+    printf("Enter Size of the Array: ");
+    scanf("%d", &size);
+    ptr = (int *)malloc(sizeof(int) * size);
+    printf("Enter %d Numbers: ", size);
+    for (i = 0; i < size; i++)
+        scanf("%d", ptr + i);
+    printf("Sum is: %d", Sum(ptr, size));
+    return 0;
+}
